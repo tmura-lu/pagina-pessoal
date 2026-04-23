3af48c3f2faf1c0a8b2097534bd85c3551de1f10
Lucca Guedes Batistela <lucca.batistela2@estudante.ufla.br>
2026-04-22 22:40:45 -0300

    feature: js do tailwind e remoção do css

---
+++ tailwind/assets/js/script.js
@@ -0,0 +1,27 @@
+var html = document.documentElement;
+var btn = document.getElementById("themeToggle");
+
+function applyTheme(theme) {
+  if (theme === "dark") {
+    html.classList.add("dark"); // Tailwind usa classe dark
+  } else {
+    html.classList.remove("dark");
+  }
+  localStorage.setItem("theme", theme);
+
+  if (btn) {
+    btn.innerHTML =
+      theme === "dark"
+        ? '<i class="fa-solid fa-sun"></i>'
+        : '<i class="fa-solid fa-moon"></i>';
+  }
+}
+
+applyTheme(localStorage.getItem("theme") || "light");
+
+if (btn) {
+  btn.addEventListener("click", function () {
+    var isDark = html.classList.contains("dark");
+    applyTheme(isDark ? "light" : "dark");
+  });
+}
